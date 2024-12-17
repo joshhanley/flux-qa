2f5c333d87e6eba06d16c203bac4d5341d871166
Josh Hanley <josh@hitsystems.com.au>
2024-12-17 19:54:57 +1100

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -2,12 +2,47 @@
 
 use Livewire\Volt\Component;
 
-new class extends Component
-{
+new class extends Component {
     //
 };
 ?>
 
-<div>
-    
+<div class="mt-4 space-y-4">
+    <flux:button inset>Button with inset</flux:button>
+    <flux:button inset href="#">Button with inset and href</flux:button>
+    {{-- <flux:button>Test</flux:button>
+    <flux:button href="/">Test</flux:button> --}}
+
+    <div class="flex">
+        Some content
+
+        <flux:button size="sm" icon="x-mark" variant="ghost" class="!inline-flex" inset />
+
+        End content - Button Inline Flex
+    </div>
+
+    <div class="flex">
+        Some content
+
+        <flux:button size="sm" icon="x-mark" variant="ghost" class="!flex" inset />
+        End content - Button Flex
+    </div>
+
+    <div class="flex">
+        Some content
+
+        <flux:button href="#" size="sm" icon="x-mark" variant="ghost" class="!inline-flex" inset />
+
+        End content - Link Inline Flex
+    </div>
+
+    <div class="flex">
+        Some content
+
+        <flux:button href="#" size="sm" icon="x-mark" variant="ghost" class="!flex" inset />
+        End content - Link Flex
+    </div>
+
+    {{-- <div style="background: lightblue; display: flex; margin-top: -10px;">A</div>
+    <div style="background: lightcoral;">B</div> --}}
 </div>
