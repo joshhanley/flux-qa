1a1e7019d36accbfa514dc7a167e5f27d8c30573
Josh Hanley <josh@hitsystems.com.au>
2024-12-20 16:06:09 +1100

    What Livewire folder for changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                'vendor/livewire/livewire/dist/**',
+            ],
         }),
         flux(),
     ],
