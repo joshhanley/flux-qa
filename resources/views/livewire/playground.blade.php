bb6ec015aa9f0f46be1e28443d07ab757468b458
Josh Hanley <josh@hitsystems.com.au>
2025-01-20 13:37:42 +1100

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -3,13 +3,46 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public $state;
-};
+    public $types = [
+        'type1' => 'Type 1',
+        'type2' => 'Type 2',
+        'type3' => 'Type 3',
+    ];
 
+    public $sites = [
+        ['value' => 1, 'label' => 'Site 1'],
+        ['value' => 2, 'label' => 'Site 2'],
+        ['value' => 3, 'label' => 'Site 3'],
+    ];
+};
 ?>
 
 <div>
-    {{ var_export($state) }}
-    <flux:checkbox wire:model="state" label="My Checkbox" :checked="old('mycheckbox', request()->input('mycheckbox'))"></flux:checkbox>
-        <button type="button" wire:click="$refresh">Refresh</button>
+    <flux:field>
+        <flux:label>Choose Type</flux:label>
+        <flux:select wire:model="form.type" :filter="false" placeholder="Choose type">
+            @foreach ($types as $value => $label)
+                <flux:option value="{{ $value }}">
+                    {{ $label }}
+                </flux:option>
+            @endforeach
+        </flux:select>
+        @error('form.type')
+        <flux:error name="form.type"/>
+        @enderror
+    </flux:field>
+
+    <flux:field>
+        <flux:label>Choose Site</flux:label>
+        <flux:select wire:model="form.site_id" :filter="false" placeholder="Choose site">
+            @foreach ($sites as $site)
+                <flux:option value="{{ $site['value'] }}">
+                    {{ $site['label'] }}
+                </flux:option>
+            @endforeach
+        </flux:select>
+        @error('form.site_id')
+        <flux:error name="form.site_id"/>
+        @enderror
+    </flux:field>
 </div>
