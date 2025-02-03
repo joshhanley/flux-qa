e9ef42b2234ce5fe44ecb59cfa303d1b1ba24c00
Josh Hanley <josh@hitsystems.com.au>
2025-02-03 17:03:05 +1100

    Reset playground

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -8,9 +8,5 @@ new class extends Component {
 ?>
 
 <div>
-    <flux:select variant="listbox" multiple>
-        @foreach (range('A', 'Z') as $value)
-            <flux:option value="{{ $value }}">{{ $value }}</flux:option>
-        @endforeach
-    </flux:select>
+    
 </div>
