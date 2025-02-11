308bd76884265738b4472c9505dc147f3ed94c04
Josh Hanley <josh@hitsystems.com.au>
2025-02-12 09:04:43 +1100

    Switch to using Tailwind Vite plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-import './bootstrap';
 // import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 
 // console.log('start')
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import flux from './flux.plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
                 'vendor/livewire/livewire/dist/**',
             ],
         }),
+        tailwindcss(),
         flux(),
     ],
 });
