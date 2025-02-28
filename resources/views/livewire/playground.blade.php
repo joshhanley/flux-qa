67284b787a8cfa8f2891b3f21eefda7a9e091aa7
Josh Hanley <josh@hitsystems.com.au>
2025-02-28 13:27:10 +1100

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -3,16 +3,44 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
-}; ?>
+    public $items = [
+        'a' => 'a',
+        'b' => 'b',
+    ];
+
+    public function prependItem()
+    {
+        $this->items = ['z' => 'z'] + $this->items;
+    }
+
+    public function appendItem()
+    {
+        $this->items['c'] = 'c';
+    }
+
+    public function removeFirstItem()
+    {
+        unset($this->items[array_key_first($this->items)]);
+    }
+};
+?>
 
 <div>
-    <flux:date-picker>
-        <x-slot name="trigger">
-            <flux:date-picker.input clearable />
-        </x-slot>
-    </flux:date-picker>
-    <flux:select variant="listbox" clearable>
-        <flux:select.option>Test</flux:select.option>
-    </flux:select>
+    <button wire:click="prependItem">Prepend Item</button>
+    <button wire:click="appendItem">Append Item</button>
+
+    <div>
+        @foreach ($items as $key => $value)
+            <div wire:key="{{ $key }}">
+                <flux:editor />
+                <flux:select variant="listbox" placeholder="Choose industry...">
+                    <flux:select.option :value="$value . '-1'">Photography</flux:select.option>
+                    <flux:select.option :value="$value . '-2'">Other</flux:select.option>
+                </flux:select>
+                <p>Value: {{ $value }}</p>
+            </div>
+        @endforeach
+    </div>
+
+    <flux:button wire:click="removeFirstItem">Remove First Item</flux:button>
 </div>
--- resources/views/livewire/sample.blade.php
+++ resources/views/livewire/sample.blade.php
@@ -1,11 +1,22 @@
 <?php
 
+use Livewire\Attributes\Lazy;
 use Livewire\Volt\Component;
 
-new class extends Component {
-    //
+new
+#[Lazy]
+class extends Component {
+    public string $tab = 'two';
 }; ?>
 
 <div>
-    <flux:calendar />
+    <flux:tab.group>
+        <flux:tabs wire:model="tab">
+            <flux:tab name="one">One</flux:tab>
+            <flux:tab name="two">Two</flux:tab>
+        </flux:tabs>
+        <flux:tab.panel name="one">One</flux:tab.panel>
+        <flux:tab.panel name="two">Two</flux:tab.panel>
+    </flux:tab.group>
 </div>
+
