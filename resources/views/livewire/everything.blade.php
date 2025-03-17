043b49e2bd6957903f33106bb3221e93f389e6d9
Josh Hanley <josh@hitsystems.com.au>
2025-03-17 18:53:36 +1100

    wip

--- resources/views/livewire/everything.blade.php
+++ resources/views/livewire/everything.blade.php
@@ -1446,4 +1446,290 @@ new class extends Component {
             <p>We've improved our notification system to deliver alerts faster and more reliably.</p>
         </flux:callout.text>
     </flux:callout>
+    
+
+    <flux:avatar tooltip="Caleb Porzio" src="https://unavatar.io/x/calebporzio" />
+
+    <!-- Or infer from the name prop... -->
+    <flux:avatar tooltip name="Caleb Porzio" src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar name="Caleb Porzio" />
+    <flux:avatar name="calebporzio" />
+    <flux:avatar name="calebporzio" initials:single />
+
+    <!-- Or use the initials prop directly... -->
+    <flux:avatar initials="CP" />
+
+    <!-- Extra large: size-16 (64px) -->
+    <flux:avatar size="xl" src="https://unavatar.io/x/calebporzio" />
+
+    <!-- Large: size-12 (48px) -->
+    <flux:avatar size="lg" src="https://unavatar.io/x/calebporzio" />
+
+    <!-- Default: size-10 (40px) -->
+    <flux:avatar src="https://unavatar.io/x/calebporzio" />
+
+    <!-- Small: size-8 (32px) -->
+    <flux:avatar size="sm" src="https://unavatar.io/x/calebporzio" />
+
+    <!-- Extra small: size-6 (24px) -->
+    <flux:avatar size="xs" src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar icon="user" />
+    <flux:avatar icon="phone" />
+    <flux:avatar icon="computer-desktop" />
+
+    <flux:avatar name="Caleb Porzio" color="red" />
+    <flux:avatar name="Caleb Porzio" color="orange" />
+    <flux:avatar name="Caleb Porzio" color="amber" />
+    <flux:avatar name="Caleb Porzio" color="yellow" />
+    <flux:avatar name="Caleb Porzio" color="lime" />
+    <flux:avatar name="Caleb Porzio" color="green" />
+    <flux:avatar name="Caleb Porzio" color="emerald" />
+    <flux:avatar name="Caleb Porzio" color="teal" />
+    <flux:avatar name="Caleb Porzio" color="cyan" />
+    <flux:avatar name="Caleb Porzio" color="sky" />
+    <flux:avatar name="Caleb Porzio" color="blue" />
+    <flux:avatar name="Caleb Porzio" color="indigo" />
+    <flux:avatar name="Caleb Porzio" color="violet" />
+    <flux:avatar name="Caleb Porzio" color="purple" />
+    <flux:avatar name="Caleb Porzio" color="fuchsia" />
+    <flux:avatar name="Caleb Porzio" color="pink" />
+    <flux:avatar name="Caleb Porzio" color="rose" />
+
+    <flux:avatar name="Caleb Porzio" color="auto" />
+
+    <!-- Use color:seed to generate a consistent color based -->
+    <!-- on something unchanging like a user's ID... -->
+    <flux:avatar name="Caleb Porzio" color="auto" color:seed="1" />
+
+    <flux:avatar circle src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar badge badge:color="green" badge:circle src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar badge badge:color="zinc" badge:position="top right" badge:circle badge:variant="outline" src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar badge="25" src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar circle badge="👍" badge:circle src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar circle src="https://unavatar.io/x/calebporzio">
+        <x-slot:badge>
+            <img class="size-3" src="https://unavatar.io/github/hugosaintemarie" />
+        </x-slot:badge>
+    </flux:avatar>
+
+    <flux:avatar.group>
+        <flux:avatar src="https://unavatar.io/x/calebporzio" />
+        <flux:avatar src="https://unavatar.io/github/hugosaintemarie" />
+        <flux:avatar src="https://unavatar.io/github/joshhanley" />
+        <flux:avatar>3+</flux:avatar>
+    </flux:avatar.group>
+
+    <!-- Adapt rings to custom background... -->
+    <flux:avatar.group class="**:ring-zinc-100 dark:**:ring-zinc-800">
+        <flux:avatar circle src="https://unavatar.io/x/calebporzio" />
+        <flux:avatar circle src="https://unavatar.io/github/hugosaintemarie" />
+        <flux:avatar circle src="https://unavatar.io/github/joshhanley" />
+        <flux:avatar circle>3+</flux:avatar>
+    </flux:avatar.group>
+
+    <flux:avatar as="button" src="https://unavatar.io/x/calebporzio" />
+
+    <flux:avatar href="https://x.com/calebporzio" src="https://unavatar.io/x/calebporzio" />
+
+    <div>
+        <div class="flex items-center gap-2">
+            <flux:icon.star variant="solid" />
+            <flux:icon.star variant="solid" />
+            <flux:icon.star variant="solid" />
+            <flux:icon.star variant="solid" />
+            <flux:icon.star variant="solid" />
+        </div>
+
+        <flux:heading size="xl" class="mt-4 italic">
+            <p>IMO Livewire takes Blade to the next level. It's basically what Blade should be by default. 🔥</p>
+        </flux:heading>
+
+        <div class="mt-6 flex items-center gap-4">
+            <flux:avatar size="lg" src="https://unavatar.io/x/taylorotwell" />
+
+            <div>
+                <flux:heading size="lg">Taylor Otwell</flux:heading>
+                <flux:text>Creator of Laravel</flux:text>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <flux:heading size="xl">The Laravel Podcast <flux:badge inset="top bottom" class="ml-1 max-sm:hidden">New</flux:badge></flux:heading>
+
+        <flux:subheading>
+            A podcast about Laravel, development best practices, and the PHP ecosystem—hosted by Jeffrey Way, Matt Stauffer, and Taylor Otwell, later joined by Adam Wathan.
+        </flux:subheading>
+
+        <flux:avatar.group class="mt-6">
+            <flux:avatar circle size="lg" src="https://unavatar.io/x/taylorotwell" />
+            <flux:avatar circle size="lg" src="https://unavatar.io/x/adamwathan" />
+            <flux:avatar circle size="lg" src="https://unavatar.io/x/jeffrey_way" />
+            <flux:avatar circle size="lg" src="https://unavatar.io/x/stauffermatt" />
+        </flux:avatar.group>
+    </div>
+
+    <flux:card>
+        <div class="flex justify-between items-center">
+            <flux:heading>Assignees</flux:heading>
+            <flux:button size="sm" variant="subtle" icon="plus" inset="top bottom" />
+        </div>
+
+        <flux:separator class="mt-2 mb-4" variant="subtle" />
+
+        <ul class="flex flex-col gap-3">
+            <li class="flex items-center gap-2">
+                <flux:avatar size="xs" src="https://unavatar.io/github/calebporzio" />
+                <flux:heading>Caleb Porzio</flux:heading>
+            </li>
+            <li class="flex items-center gap-2">
+                <flux:avatar size="xs" src="https://unavatar.io/github/hugosaintemarie" />
+                <flux:heading>Hugo Sainte-Marie</flux:heading>
+            </li>
+            <li class="flex items-center gap-2">
+                <flux:avatar size="xs" src="https://unavatar.io/github/joshhanley" />
+                <flux:heading>Josh Hanley</flux:heading>
+            </li>
+            <li class="flex items-center gap-2">
+                <flux:avatar size="xs" src="https://unavatar.io/github/jasonlbeggs" />
+                <flux:heading>Jason Beggs</flux:heading>
+            </li>
+        </ul>
+    </flux:card>
+
+    <flux:select variant="listbox" label="Assign to">
+        <flux:select.option selected>
+            <div class="flex items-center gap-2 whitespace-nowrap">
+                <flux:avatar circle size="xs" src="https://unavatar.io/github/calebporzio" /> Caleb Porzio
+            </div>
+        </flux:select.option>
+
+        <flux:select.option>
+            <div class="flex items-center gap-2 whitespace-nowrap">
+                <flux:avatar circle size="xs" src="https://unavatar.io/github/hugosaintemarie" /> Hugo Sainte-Marie
+            </div>
+        </flux:select.option>
+
+        <flux:select.option>
+            <div class="flex items-center gap-2 whitespace-nowrap">
+                <flux:avatar circle size="xs" src="https://unavatar.io/github/joshhanley" /> Josh Hanley
+            </div>
+        </flux:select.option>
+
+        <flux:select.option>
+            <div class="flex items-center gap-2 whitespace-nowrap">
+                <flux:avatar circle size="xs" src="https://unavatar.io/github/jasonlbeggs" /> Jason Beggs
+            </div>
+        </flux:select.option>
+    </flux:select>
+
+    <flux:dropdown hover position="bottom center">
+        <flux:avatar as="button" name="calebporzio" src="https://unavatar.io/x/calebporzio" />
+
+        <flux:popover class="relative max-w-[15rem]">
+            <div class="absolute top-0 right-0 p-2">
+                <flux:button icon="user-plus" variant="filled" size="sm">Follow back</flux:button>
+            </div>
+
+            <flux:avatar size="xl" name="calebporzio" src="https://unavatar.io/x/calebporzio" />
+
+            <flux:heading class="mt-2">Caleb Porzio</flux:heading>
+            <flux:text>@calebporzio <flux:badge color="zinc" size="sm" inset="top bottom">Follows you</flux:badge></flux:text>
+
+            <flux:text class="mt-3">
+                I'm a full stack developer with a passion for building web applications. Currently working on a new project called <flux:link href="https://fluxui.dev">Flux</flux:link>.
+            </flux:text>
+
+            <div class="flex gap-4 mt-3">
+                <div class="flex gap-2 items-center">
+                    <flux:heading>1.2k</flux:heading> <flux:text>Followers</flux:text>
+                </div>
+
+                <div class="flex gap-2 items-center">
+                    <flux:heading>1.2k</flux:heading> <flux:text>Following</flux:text>
+                </div>
+            </div>
+        </flux:popover>
+    </flux:dropdown>
+
+
+    <flux:profile src="https://unavatar.io/x/calebporzio" />
+
+    <flux:profile avatar="https://unavatar.io/x/calebporzio" />
+
+    <flux:profile :chevron="false" avatar="https://unavatar.io/x/calebporzio" />
+
+    <flux:profile circle :chevron="false" avatar="https://unavatar.io/x/calebporzio" />
+
+    <flux:profile circle name="Caleb Porzio" avatar="https://unavatar.io/x/calebporzio" />
+
+    <!-- Automatically generates initials from name -->
+    <flux:profile name="Caleb Porzio" />
+
+    <!-- Specify color... -->
+    <flux:profile name="Caleb Porzio" avatar:color="cyan" />
+
+    <!-- Manually specify initials... -->
+    <flux:profile initials="CP" />
+
+    <!-- Provide name only for avatar initial generation... -->
+    <flux:profile avatar:name="Caleb Porzio" />
+
+    <flux:profile
+        icon:trailing="chevron-up-down"
+        avatar="https://unavatar.io/x/calebporzio"
+        name="Caleb Porzio"
+    />
+
+    <flux:dropdown align="end">
+        <flux:profile avatar="https://unavatar.io/x/calebporzio" />
+
+        <flux:navmenu class="max-w-[12rem]">
+            <div class="px-2 py-1.5">
+                <flux:subheading size="sm">Signed in as</flux:subheading>
+                <flux:heading class="mt-1! truncate">caleb@example.com</flux:heading>
+            </div>
+
+            <flux:navmenu.separator />
+
+            <div class="px-2 py-1.5">
+                <flux:subheading size="sm" class="pl-7">Teams</flux:subheading>
+            </div>
+
+            <flux:navmenu.item href="#" icon="check" class="text-zinc-800 dark:text-white truncate">Personal</flux:navmenu.item>
+            <flux:navmenu.item href="#" indent class="text-zinc-800 dark:text-white truncate">Wireable LLC</flux:navmenu.item>
+
+            <flux:navmenu.separator />
+
+            <flux:navmenu.item href="/dashboard" icon="key" class="text-zinc-800 dark:text-white">Licenses</flux:navmenu.item>
+            <flux:navmenu.item href="/account" icon="user" class="text-zinc-800 dark:text-white">Account</flux:navmenu.item>
+
+            <flux:navmenu.separator />
+
+            <flux:navmenu.item href="/logout" icon="arrow-right-start-on-rectangle" class="text-zinc-800 dark:text-white">Logout</flux:navmenu.item>
+        </flux:navmenu>
+    </flux:dropdown>
+
+    <flux:dropdown position="top" align="start">
+        <flux:profile avatar="https://unavatar.io/x/calebporzio" name="Caleb Porzio" />
+
+        <flux:menu>
+            <flux:menu.radio.group>
+                <flux:menu.radio checked>Caleb Porzio</flux:menu.radio>
+                <flux:menu.radio>Hugo Sainte-Marie</flux:menu.radio>
+                <flux:menu.radio>Josh Hanley</flux:menu.radio>
+            </flux:menu.radio.group>
+
+            <flux:menu.separator />
+
+            <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+        </flux:menu>
+    </flux:dropdown>
 </div>
--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -10,18 +10,9 @@ class extends Component {
 }; ?>
 
 <div>
-    <flux:callout variant="secondary" icon="information-circle" heading="Your account has been successfully created." />
-    <flux:callout variant="success" icon="check-circle" heading="Your account is verified and ready to use." />
-    <flux:callout variant="warning" icon="exclamation-circle" heading="Please verify your account to unlock all features." />
-    <flux:callout variant="danger" icon="x-circle" heading="Something went wrong. Try again or contact support." />
+    <flux:profile avatar="https://unavatar.io/x/calebporzio" />
 
-    
-    <flux:profile src="https://unavatar.io/x/calebporzio" />
-    {{-- <flux:date-picker mode="range" value="2025-03-29/2025-04-08" with-presets with-confirmation week-numbers>
-        <x-slot name="trigger">
-            <flux:date-picker.input />
-        </x-slot>
-    </flux:date-picker>
-    <input type="date" /> --}}
-    {{-- <flux:date-picker /> --}}
+    <flux:profile name="Caleb Porzio" avatar="https://unavatar.io/x/calebporzio" />
+
+    <flux:profile :chevron="false" avatar="https://unavatar.io/x/calebporzio" />
 </div>
