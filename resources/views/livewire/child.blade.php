8cdf89b9de71ab685fe9ffb58844306660359e94
Josh Hanley <josh@hitsystems.com.au>
2025-04-14 18:02:55 +1000

    livewire morph snapshot issues

--- resources/views/livewire/child.blade.php
+++ resources/views/livewire/child.blade.php
@@ -4,7 +4,10 @@ use App\Models\User;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public $number;
 }; ?>
 
-<div>Child</div>
+<div>
+    Child {{ $number }}
+    <button wire:click="$refresh">Refresh</button>
+</div>
