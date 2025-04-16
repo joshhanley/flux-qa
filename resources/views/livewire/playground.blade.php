100d8f79cf204292b12cdd04fa2cba1c77f14797
Josh Hanley <josh@hitsystems.com.au>
2025-04-16 15:25:42 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Models\User;
-use Flux\Flux;
-use Livewire\Attributes\Computed;
-use Livewire\Attributes\On;
-use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 
 new class extends Component
 {
-    
+    //
 };
 ?>
 
