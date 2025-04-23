1a62986a6d7bb62d2e1b6d4708691523e66a9cef
Josh Hanley <josh@hitsystems.com.au>
2025-04-23 14:10:49 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -11,43 +11,35 @@ new class extends Component
     public $content = '';
     public $images = [];
 
-    public function uploadIt(
-        $files
-    )
+    public function uploadIt($fileData)
     {
-        ray($files);
+        ray($fileData);
 
-        return 'bob';
-    }
+        Storage::disk('public')->put('something.png', $fileData[0]);
 
-    public static function saveEditorImages(
-        #[SupportFileUploads] $images
-    )
-    {
-        $imageUrls = [];
-
-        foreach ($images as $image) {
-            $imageUrls[] = $image->store('images', 'public');
-        }
-
-        return $imageUrls;
-    }
-
-    public function saveImages()
-    {
-        $response = [];
-
-        foreach ($this->images as $image) {
-            $response[] = $image->store('images', 'public');
-        }
-
-        $this->images = [];
-
-        return $response;
+        ray('done!');
     }
 };
 ?>
 
-<div>
-    <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" class="[&_[data-slot=content]]:min-h-[100px]" />
+<div x-data="{
+    async uploadFile() {
+        console.log(this.$refs.file);
+        let file = this.$refs.file.files[0];
+
+        console.log(file, file.text());
+
+        let fileText = await file.arrayBuffer();
+
+        fileText = new Uint8Array(fileText);
+
+        console.log(fileText);
+
+        $wire.uploadIt([fileText, file.type]);        
+    }
+}">
+    <flux:button icon:trailing="chevron-down">Open</flux:button>
+    <input type="file" x-ref="file" />
+    <button x-on:click="uploadFile()">Upload</button>
+    {{-- <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" class="[&_[data-slot=content]]:min-h-[100px]" /> --}}
 </div>
