8a07ab34d87f5409802fe9f3323617a8f6b3409b
Josh Hanley <josh@hitsystems.com.au>
2025-04-28 11:58:38 +1000

    Use latest editor API

--- resources/js/app.js
+++ resources/js/app.js
@@ -163,167 +163,247 @@
 
 
 // --- WORKING IMPLEMENTATION OF IMAGES WITH EDITOR EXTENSION -- //
+// import Image from '@tiptap/extension-image'
+// import FileHandler from '@tiptap-pro/extension-file-handler'
+
+// document.addEventListener('flux:editor', (e) => {
+//     // e.detail.disableExtension('underline')
+//     // e.detail.enableExtension('image')
+//     e.detail.registerExtension(Image.extend({
+//         addAttributes() {
+//             return {
+//                 ...this.parent?.(),
+//                 'data-upload-id': {
+//                     default: undefined,
+//                 },
+//             }
+//         },
+//     }))
+
+//     e.detail.initializeToolbarButton(({editor, toolbar, setActiveState}) => {
+//         if (toolbar.querySelector('[data-editor="image"]')) {
+//             toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('keydown', e => {
+//                 if (['ArrowLeft', 'ArrowRight'].includes(e.key) || /^[a-zA-Z0-9]$/.test(e.key)) {
+//                     e.stopPropagation();
+//                 }
+//             })
+
+//             toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('input', e => e.stopPropagation())
+//             toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('change', e => e.stopPropagation())
+
+//             let insertImage = () => {
+//                 let url = toolbar.querySelector('[data-editor="image:url"]')?.value?.trim();
+
+//                 if (!url) return;
+
+//                 editor.chain().focus().setImage({ src: url }).run();
+
+//                 if (toolbar.querySelector('[data-editor="image:url"]')) {
+//                     toolbar.querySelector('[data-editor="image:url"]').value = '';
+//                 }
+//             }
+
+//             toolbar.querySelector('[data-editor="image:insert"]')?.addEventListener('click', insertImage)
+//             toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('keydown', e => ['Enter'].includes(e.key) && insertImage())
+
+//         }
+
+//         setActiveState(() => {
+//             let imageInput = toolbar.querySelector('[data-editor="image:url"]')
+
+//             if (imageInput) {
+//                 let imageButton = toolbar.querySelector('[data-editor="image"] [data-match-target]')
+
+//                 if (editor.isActive('image')) {
+//                     imageButton && imageButton.setAttribute('data-match', '')
+
+//                     let attrs = editor.getAttributes('image')
+//                     imageInput.value = attrs.src || ''
+//                 } else {
+//                     imageButton && imageButton.removeAttribute('data-match')
+//                     imageInput.value = ''
+//                 }
+//             }
+//         })
+//     })
+// })
+
+// document.addEventListener('flux:editor', (e) => {
+//     e.detail.registerExtension(FileHandler.configure({
+//         // allowedMimeTypes: [],
+//         onPaste: (editor, files, htmlContent) => {
+//             let pos = editor.state.selection
+
+//             // If there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
+//             // you could extract the pasted file from this url string and upload it to a server for example...
+//             if (htmlContent) return
+
+//             uploadImages(editor, files, pos)
+//         },
+//         onDrop: (editor, files, pos) => {
+//             uploadImages(editor, files, pos)
+//         },
+//     }))
+// })
+
+// let uploadImages = (editor, files, pos) => {
+//     let editorEl = editor.options.element
+
+//     if (!editorEl) return;
+
+//     let component = editorEl.closest('[wire\\:id]')?.__livewire
+
+//     if (!component) return;
+
+//     let id = addPlaceholder(editor, pos)
+
+//     let $wire = component.$wire
+
+//     $wire.uploadMultiple(
+//         'images',
+//         files,
+//         async () => {
+//             // Once the upload is finished, we need to save the images to a publically accessible location...
+//             console.log(files)
+
+//             let formData = new FormData()
+//             files.forEach(file => {
+//                 formData.append('images[]', file)
+//             })
+
+//             let imageUrls = await $wire.uploadIt({files: formData})
+
+//             replacePlaceholder(editor, id, imageUrls)
+//         },
+//         () => console.log('error'),
+//         event => console.log('progress', event),
+//         () => console.log('cancelled')
+//     )
+// }
+
+// let addPlaceholder = (editor, pos) => {
+//     let placeholderSrc = 'https://placehold.co/100x50?text=Uploading...'
+//     let id = 'editor-image-' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+
+//     editor
+//         .chain()
+//         .focus()
+//         .insertContentAt(pos,{
+//             type: 'image',
+//             attrs: { src: placeholderSrc, 'data-upload-id': id },
+//         })
+//         .run()
+
+//     return id
+// }
+
+// let replacePlaceholder = (editor, id, imageUrls) => {
+//     let foundNode = null
+//     let nodePos = null
+
+//     editor.state.doc.descendants((node, posHere) => {
+//         if (node.type.name === 'image' && node.attrs['data-upload-id'] === id) {
+//             foundNode = node
+//             nodePos = posHere
+//             return false
+//         }
+//         return true
+//     })
+
+//     if (foundNode === null || nodePos === null) return
+
+//     // If there are no images, just delete the placeholder image...
+//     if (!imageUrls || imageUrls.length === 0) {
+//         editor.chain().focus().deleteRange({ from: nodePos, to: nodePos + foundNode.nodeSize }).run()
+
+//         return
+//     }
+
+//     let images = imageUrls.map(src => ({
+//         type: 'image',
+//         attrs: { src }
+//     }))
+
+//     editor.chain().focus().insertContentAt({ from: nodePos, to: nodePos + foundNode.nodeSize }, images).run()
+// }
+// --- END OF WORKING IMPLEMENTATION OF IMAGES WITH EDITOR EXTENSION -- //
+
+// --- START OF FINAL IMPLEMENTATION --- //
+
 import Image from '@tiptap/extension-image'
-import FileHandler from '@tiptap-pro/extension-file-handler'
 
 document.addEventListener('flux:editor', (e) => {
-    // e.detail.disableExtension('underline')
-    // e.detail.enableExtension('image')
-    e.detail.registerExtension(Image.extend({
-        addAttributes() {
-            return {
-                ...this.parent?.(),
-                'data-upload-id': {
-                    default: undefined,
-                },
-            }
-        },
-    }))
+    e.detail.disableExtension('underline')
+    e.detail.registerExtension(Image)
 
-    e.detail.initializeToolbarButton(({editor, toolbar, setActiveState}) => {
-        if (toolbar.querySelector('[data-editor="image"]')) {
-            toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('keydown', e => {
+    e.detail.init(({ editor }) => {
+        editor.on('contentError', ({ editor }) => {})
+
+        editor.on('create', ({ editor }) => {})
+
+        editor.on('update', ({ editor }) => {})
+
+        editor.on('selectionUpdate', ({ editor }) => {})
+
+        editor.on('transaction', ({ editor }) => {})
+
+        editor.on('focus', ({ editor }) => {})
+
+        editor.on('blur', ({ editor }) => {})
+
+        editor.on('destroy', ({ editor }) => {})
+
+        editor.on('drop', ({ editor }) => {})
+
+        editor.on('paste', ({ editor }) => {})
+    })
+
+    e.detail.init(({ editor }) => {
+        editor.on('create', ({ editor }) => {
+            let toolbar = editor.options.element.closest('ui-editor').querySelector('ui-toolbar')
+
+            let imageToolbarElement = toolbar.querySelector('[data-editor="image"]')
+
+            if (! imageToolbarElement) return
+
+            let toolbarButton = imageToolbarElement.querySelector('[data-match-target]')
+            let input = imageToolbarElement.querySelector('[data-editor="image:url"]')
+            let insertButton = imageToolbarElement.querySelector('[data-editor="image:insert"]')
+
+            if (! toolbarButton || ! input) return
+
+            let insertImage = () => {
+                let url = input.value?.trim();
+
+                if (url === '') return;
+
+                editor.chain().focus().setImage({ src: url }).run();
+
+                input.value = '';
+            }
+
+            input.addEventListener('keydown', e => {
                 if (['ArrowLeft', 'ArrowRight'].includes(e.key) || /^[a-zA-Z0-9]$/.test(e.key)) {
                     e.stopPropagation();
                 }
             })
+            input.addEventListener('input', e => e.stopPropagation())
+            input.addEventListener('change', e => e.stopPropagation())
+            input.addEventListener('keydown', e => ['Enter'].includes(e.key) && insertImage())
 
-            toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('input', e => e.stopPropagation())
-            toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('change', e => e.stopPropagation())
+            insertButton.addEventListener('click', insertImage)
 
-            let insertImage = () => {
-                let url = toolbar.querySelector('[data-editor="image:url"]')?.value?.trim();
-
-                if (!url) return;
-
-                editor.chain().focus().setImage({ src: url }).run();
-
-                if (toolbar.querySelector('[data-editor="image:url"]')) {
-                    toolbar.querySelector('[data-editor="image:url"]').value = '';
-                }
-            }
-
-            toolbar.querySelector('[data-editor="image:insert"]')?.addEventListener('click', insertImage)
-            toolbar.querySelector('[data-editor="image:url"]')?.addEventListener('keydown', e => ['Enter'].includes(e.key) && insertImage())
-
-        }
-
-        setActiveState(() => {
-            let imageInput = toolbar.querySelector('[data-editor="image:url"]')
-
-            if (imageInput) {
-                let imageButton = toolbar.querySelector('[data-editor="image"] [data-match-target]')
-
+            editor.on('transaction', ({ editor }) => {
                 if (editor.isActive('image')) {
-                    imageButton && imageButton.setAttribute('data-match', '')
+                    toolbarButton.setAttribute('data-match', '')
 
                     let attrs = editor.getAttributes('image')
-                    imageInput.value = attrs.src || ''
+                    input.value = attrs.src || ''
                 } else {
-                    imageButton && imageButton.removeAttribute('data-match')
-                    imageInput.value = ''
+                    toolbarButton.removeAttribute('data-match')
+                    input.value = ''
                 }
-            }
-        })
-    })
-})
-
-document.addEventListener('flux:editor', (e) => {
-    e.detail.registerExtension(FileHandler.configure({
-        // allowedMimeTypes: [],
-        onPaste: (editor, files, htmlContent) => {
-            let pos = editor.state.selection
-
-            // If there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
-            // you could extract the pasted file from this url string and upload it to a server for example...
-            if (htmlContent) return
-
-            uploadImages(editor, files, pos)
-        },
-        onDrop: (editor, files, pos) => {
-            uploadImages(editor, files, pos)
-        },
-    }))
-})
-
-let uploadImages = (editor, files, pos) => {
-    let editorEl = editor.options.element
-
-    if (!editorEl) return;
-
-    let component = editorEl.closest('[wire\\:id]')?.__livewire
-
-    if (!component) return;
-
-    let id = addPlaceholder(editor, pos)
-
-    let $wire = component.$wire
-
-    $wire.uploadMultiple(
-        'images',
-        files,
-        async () => {
-            // Once the upload is finished, we need to save the images to a publically accessible location...
-            console.log(files)
-
-            let formData = new FormData()
-            files.forEach(file => {
-                formData.append('images[]', file)
             })
-
-            let imageUrls = await $wire.uploadIt({files: formData})
-
-            replacePlaceholder(editor, id, imageUrls)
-        },
-        () => console.log('error'),
-        event => console.log('progress', event),
-        () => console.log('cancelled')
-    )
-}
-
-let addPlaceholder = (editor, pos) => {
-    let placeholderSrc = 'https://placehold.co/100x50?text=Uploading...'
-    let id = 'editor-image-' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-
-    editor
-        .chain()
-        .focus()
-        .insertContentAt(pos,{
-            type: 'image',
-            attrs: { src: placeholderSrc, 'data-upload-id': id },
         })
-        .run()
-
-    return id
-}
-
-let replacePlaceholder = (editor, id, imageUrls) => {
-    let foundNode = null
-    let nodePos = null
-
-    editor.state.doc.descendants((node, posHere) => {
-        if (node.type.name === 'image' && node.attrs['data-upload-id'] === id) {
-            foundNode = node
-            nodePos = posHere
-            return false
-        }
-        return true
     })
-
-    if (foundNode === null || nodePos === null) return
-
-    // If there are no images, just delete the placeholder image...
-    if (!imageUrls || imageUrls.length === 0) {
-        editor.chain().focus().deleteRange({ from: nodePos, to: nodePos + foundNode.nodeSize }).run()
-
-        return
-    }
-
-    let images = imageUrls.map(src => ({
-        type: 'image',
-        attrs: { src }
-    }))
-
-    editor.chain().focus().insertContentAt({ from: nodePos, to: nodePos + foundNode.nodeSize }, images).run()
-}
-// --- END OF WORKING IMPLEMENTATION OF IMAGES WITH EDITOR EXTENSION -- //
+})
--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -1,45 +1,17 @@
 <?php
 
+use App\Models\User;
+use Livewire\Attributes\Computed;
 use Livewire\Volt\Component;
-use Livewire\WithFileUploads;
-use Livewire\Attributes\SupportFileUploads;
 
-new class extends Component
-{
-    use WithFileUploads;
-
+new class extends Component {
     public $content = '';
-    public $images = [];
+}; ?>
 
-    public function uploadIt($fileData)
-    {
-        ray($fileData);
+<div>
+    <p>https://placehold.co/800x400</p>
+    <flux:text>Content: {{ var_export($content, true) }}</flux:text>
+    <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" />
 
-        Storage::disk('public')->put('something.png', $fileData[0]);
-
-        ray('done!');
-    }
-};
-?>
-
-<div x-data="{
-    async uploadFile() {
-        console.log(this.$refs.file);
-        let file = this.$refs.file.files[0];
-
-        console.log(file, file.text());
-
-        let fileText = await file.arrayBuffer();
-
-        fileText = new Uint8Array(fileText);
-
-        console.log(fileText);
-
-        $wire.uploadIt([fileText, file.type]);        
-    }
-}">
-    <flux:button icon:trailing="chevron-down">Open</flux:button>
-    <input type="file" x-ref="file" />
-    <button x-on:click="uploadFile()">Upload</button>
-    {{-- <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" class="[&_[data-slot=content]]:min-h-[100px]" /> --}}
+    {{-- <flux:editor toolbar:prepend="image |" /> --}}
 </div>
