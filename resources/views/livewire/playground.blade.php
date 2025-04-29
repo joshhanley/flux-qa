46375c6bba91fe393cdfd07cbe73099a3e3bc751
Josh Hanley <josh@hitsystems.com.au>
2025-04-29 10:12:52 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -8,10 +8,18 @@ new class extends Component {
     public $content = '';
 }; ?>
 
-<div>
-    <p>https://placehold.co/800x400</p>
+<div class="max-w-32">
+    {{-- <p>https://placehold.co/800x400</p>
     <flux:text>Content: {{ var_export($content, true) }}</flux:text>
-    <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" />
+    <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" /> --}}
 
     {{-- <flux:editor toolbar:prepend="image |" /> --}}
+
+    <flux:select variant="listbox" searchable wire:model="event" placeholder="Please select" label="Event" required>
+        <flux:select.option>This is just a test select box input value</flux:select.option>
+    </flux:select>
+
+    <flux:select wire:model="event" placeholder="Please select" label="Event" required>
+    <flux:select.option>This is just a test select box input value</flux:select.option>
+    </flux:select>
 </div>
--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,30 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import flux from './flux.plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
 
 export default defineConfig({
-    // server: {
-    //     host: '0.0.0.0',
-    //     port: 5173,
-    //     https: false,
-    //     // origin: 'http://192.168.4.43:5173',
-    //     cors: {
-    //         origin: 'http://192.168.4.43', // <- this is the actual origin (no path)
-    //     },
-    //     hmr: {
-    //         host: '192.168.4.43',
-    //         protocol: 'ws',
-    //         port: 5173,
-    //     },
-    // },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        // https: true,
+        https: {
+            key: fs.readFileSync('/Users/josh/Library/Application Support/Herd/config/valet/Certificates/fluxqa.test.key'),
+            cert: fs.readFileSync('/Users/josh/Library/Application Support/Herd/config/valet/Certificates/fluxqa.test.crt'),
+        },
+        // origin: 'http://192.168.4.46:5173',
+        origin: 'https://fluxqa.test:5173',
+        cors: {
+            // origin: 'http://192.168.4.46', // <- this is the actual origin (no path)
+            origin: 'https://fluxqa.test', // <- this is the actual origin (no path)
+        },
+        hmr: {
+            // host: '192.168.4.46',
+            host: 'fluxqa.test',
+            protocol: 'wss',
+            port: 5173,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
