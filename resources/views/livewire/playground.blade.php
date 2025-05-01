c9c1d4c8e74f76f240464ebff06426decdcc6a50
Josh Hanley <josh@hitsystems.com.au>
2025-05-01 15:15:59 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -1,25 +1,14 @@
 <?php
 
 use App\Models\User;
+use Flux\Flux;
 use Livewire\Attributes\Computed;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public $content = '';
+    //
 }; ?>
 
-<div class="max-w-32">
-    {{-- <p>https://placehold.co/800x400</p>
-    <flux:text>Content: {{ var_export($content, true) }}</flux:text>
-    <flux:editor wire:model.live="content" toolbar="heading | bold italic strike | bullet ordered blockquote | link | align | image" /> --}}
-
-    {{-- <flux:editor toolbar:prepend="image |" /> --}}
-
-    <flux:select variant="listbox" searchable wire:model="event" placeholder="Please select" label="Event" required>
-        <flux:select.option>This is just a test select box input value</flux:select.option>
-    </flux:select>
-
-    <flux:select wire:model="event" placeholder="Please select" label="Event" required>
-    <flux:select.option>This is just a test select box input value</flux:select.option>
-    </flux:select>
+<div>
+    
 </div>
