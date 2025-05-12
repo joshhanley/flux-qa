f20db2cb41d6eb6d831d528fab20c8fa0a8c9945
Josh Hanley <josh@hitsystems.com.au>
2025-05-12 12:16:30 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -1,14 +1,11 @@
 <?php
 
-use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new
-#[Layout('components.layouts.sidebar')]
-class extends Component {
-    public $date = '2025-07-19';
+new class extends Component {
+    //
 }; ?>
 
-<flux:main>
-    <flux:date-picker open-to="2026-01-01" wire:model2="date" value="2025-07-19" />
-</flux:main>
+<div>
+
+</div>
