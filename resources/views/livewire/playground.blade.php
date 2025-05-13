07e5d20e7bfaedd48176536eaebd9210fd0d880b
Josh Hanley <josh@hitsystems.com.au>
2025-05-13 16:54:47 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -7,5 +7,12 @@ new class extends Component {
 }; ?>
 
 <div>
-
+    <button x-on:click="$flux.toast({
+        heading: 'Success!',
+        text: 'Your changes have been saved',
+        variant: 'success',
+        duration: 3000
+    })">
+        Click me
+    </button>
 </div>
