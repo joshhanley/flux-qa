717568a4d06c3f6c8047d4fbd2febe075c69565c
Josh Hanley <josh@hitsystems.com.au>
2025-06-24 15:19:47 +1000

    wip

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -1,29 +1,10 @@
 <?php
 
-use Livewire\Attributes\On;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    #[Paginator]
-    public $posts = {
-        get => Post::paginate(10),
-    }
-
-    #[Island]
-    public function posts() {
-        return Post::paginate(10);
-    }
+    //
 } ?>
 <div>
-    <div wire:intersect="posts.loadPrevious" wire:show="posts.hasPrevious"></div>
-
-    @island('posts')
-    <div>
-        @foreach ($posts as $post)
-            <div>{{ $post->title }}</div>
-        @endforeach
-    </div>
-    @endisland
-
-    <div wire:click="posts.loadNext" wire:show="posts.hasMore"></div>
+    Playground
 </div>
