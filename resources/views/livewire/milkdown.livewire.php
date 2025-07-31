7f76b3b63cb639a603499eaaaa8b1133485775b6
Josh Hanley <josh@hitsystems.com.au>
2025-08-01 09:01:42 +1000

    wip

--- resources/views/livewire/milkdown.livewire.php
+++ resources/views/livewire/milkdown.livewire.php
@@ -13,27 +13,5 @@ new class extends Component {
 </div>
 
 <script type="module">
-    import { Crepe } from "@milkdown/crepe";
-    import "@milkdown/crepe/theme/common/style.css";
-    import "@milkdown/crepe/theme/frame.css";
-
-    document.addEventListener("DOMContentLoaded", async () => {
-      const root = document.getElementById("editor");
-      const crepe = new Crepe({
-        root,
-        defaultValue: "# Hello, Milkdown!\n\nStart editing...",
-      });
-
-      await crepe.create();
-
-      // Optionally, listen for content changes
-      crepe.on(listener => {
-        listener.markdownUpdated((ctx, markdown) => {
-          console.log("Markdown updated:", markdown);
-        });
-      });
-
-      // Expose for debugging or interaction
-      window.markdownCrepe = crepe;
-    });
+    
 </script>
